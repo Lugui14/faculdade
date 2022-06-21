a189de15a304b521c8938aab2ffe0d394ed46c6c
lab-cc <aluno@lab-cc>
2022-06-21 19:58:29 -0300

    wreid algo*

--- clube_prog/wreid-algo.py
+++ clube_prog/wreid-algo.py
@@ -4,10 +4,9 @@ while True:
     if i == 1:
         print(i)
         break
+    else: print(i, end=" ")
 
     if i % 2 == 0:
         i //= 2
     else:
         i = (i*3) + 1
-
-    print(i, end=" ")
